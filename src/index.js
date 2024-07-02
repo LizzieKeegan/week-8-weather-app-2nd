3830c26af01d082464eb22c32e692d5ff0ea39f2
LizzieKeegan <lizziekeegan16@gmail.com>
2024-07-02 15:26:33 +0700

    Forecast loop (X5) created in JS

--- src/index.js
+++ src/index.js
@@ -44,22 +44,32 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">☀️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">18°</div>
+          7°
+        </div>
+      </div>
+      `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
-
-let forecast = document.querySelector("#forecast");
-
-forecast.innerHTML = `
-  <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
-    <div class="weather-forecast-icon">☀️</div>
-    <div class="weather-forecast-temperatures">
-      <div class="weather-forecast-temperature">18°</div>
-      7°
-    </div>
-  </div>
-`;
+displayForecast();
